2f3b7d011bb7a97da7eb5d02db5186da58c1e217
Rohit9987 <rohit9987@gmail.com>
2023-08-22 22:56:25 +0400

    cleaned up

--- main.cc
+++ main.cc
@@ -14,9 +14,6 @@
 #include <fstream>
 #include <string>
 #include <map>
-#include <filesystem>       // c++17
-namespace fs = std::filesystem;
-
 
 using Map = std::map <std::string, std::string>;
 
@@ -65,56 +62,31 @@ void anonymise(Map& map_, itk::MetaDataDictionary& dictionary)
     }
 }
 
-void printDict(itk::MetaDataDictionary& dictionary)
-{
-    using MetaDataStringType = itk::MetaDataObject<std::string>;
-    auto itr = dictionary.Begin();
-    auto end = dictionary.End();
 
-    while(itr != end)
-    {
-        itk::MetaDataObjectBase::Pointer entry = itr->second;
-        
-        MetaDataStringType::Pointer entryvalue = dynamic_cast<MetaDataStringType*> (entry.GetPointer());
-
-        if(entryvalue)
-        {
-            std::string tagkey = itr->first;
-            std::string labelId;
-            bool found = itk::GDCMImageIO::GetLabelFromTag(tagkey, labelId);
-            std::string tagvalue = entryvalue->GetMetaDataObjectValue();
-
-            if(found)
-            {
-                std::cout << "(" << tagkey << ") " << labelId << " = " << tagvalue.c_str() << "\n";
-            }
-
-        }
-        ++itr;
-    }
-}
-
-int seriesAnonymiser(const char* inputDirectory, Map map_)
+int seriesAnonymiser(const char* inputDirectory, Map& map_)
 {
     using PixelType = signed short;
     constexpr unsigned int Dimension = 3;
     using ImageType = itk::Image<PixelType, Dimension>;
     using ReaderType = itk::ImageSeriesReader<ImageType>;
+    ReaderType::Pointer reader = ReaderType::New();
 
     using ImageIOType = itk::GDCMImageIO;
     ImageIOType::Pointer gdcmIO = ImageIOType::New();
+    reader->SetImageIO(gdcmIO);
 
     using NamesGeneratorType = itk::GDCMSeriesFileNames;
     auto namesGenerator = NamesGeneratorType::New();
     namesGenerator->SetInputDirectory(inputDirectory);
     const ReaderType::FileNamesContainer& filenames = namesGenerator->GetInputFileNames();
-    
-    ReaderType::Pointer reader = ReaderType::New();
-    reader->SetImageIO(gdcmIO);
     reader->SetFileNames(filenames);
 
     try{ reader->Update();}
     catch(itk::ExceptionObject& ex) { std::cerr << ex << '\n'; return -1; }
+    
+    using DictionaryType = itk::MetaDataDictionary;
+    DictionaryType& dictionary = gdcmIO->GetMetaDataDictionary();
+    anonymise(map_, dictionary);
 
     std::string datetime = itksys::SystemTools::GetCurrentDateTime("%d_%b_%Y_%H_%M_%S"); 
     std::string outputDirectory = "./output_" + datetime;
@@ -128,22 +100,11 @@ int seriesAnonymiser(const char* inputDirectory, Map map_)
     seriesWriter->SetInput(reader->GetOutput());
     seriesWriter->SetImageIO(gdcmIO);
 
-    using DictionaryType = itk::MetaDataDictionary;
-    DictionaryType& dictionary = gdcmIO->GetMetaDataDictionary();
-
-    printDict(dictionary);
-    std::cout << "AnonymizeDict\n"; 
-    anonymise(map_, dictionary);
-
-    // TODO: delete later
-    printDict(dictionary);
-     
     namesGenerator->SetOutputDirectory(outputDirectory);
     seriesWriter->SetFileNames(namesGenerator->GetOutputFileNames());
-   // seriesWriter->SetMetaDataDictionary(dictionary);
     
     try { seriesWriter->Update();}
-    catch(itk::ExceptionObject& ex) { std::cerr << ex << '\n'; return -1; }
+    catch(itk::ExceptionObject& ex) { std::cerr << ex << '\n'; return -2; }
 
     return 0;
 }
@@ -155,17 +116,16 @@ int dicomAnonymiser(const char* inputFilename, Map& map_)
     using PixelType = signed short;
     constexpr unsigned int Dimension = 2;
     using ImageType = itk::Image<PixelType, Dimension>;
-    
     using ReaderType = itk::ImageFileReader<ImageType>;
     ReaderType::Pointer reader =  ReaderType::New();
 
     using ImageIOType = itk::GDCMImageIO;
     ImageIOType::Pointer dicomIO = ImageIOType::New();
-    reader->SetFileName(inputFilename);
     reader->SetImageIO(dicomIO);
 
-    try
-    { reader->Update();}
+    reader->SetFileName(inputFilename);
+
+    try { reader->Update(); }
     catch(itk::ExceptionObject& ex) { std::cerr << ex <<std::endl; return -1; }
 
     ImageType::Pointer inputImage = reader->GetOutput();
@@ -173,25 +133,22 @@ int dicomAnonymiser(const char* inputFilename, Map& map_)
 
     using DictionaryType = itk::MetaDataDictionary;
     DictionaryType& dictionary = inputImage->GetMetaDataDictionary();
-
     anonymise(map_, dictionary);
+
+    std::string datetime = itksys::SystemTools::GetCurrentDateTime("%d_%b_%Y_%H_%M_%S"); 
+    std::string outputFilename = "./output_" + datetime + ".dcm";
 
     using WriterType = itk::ImageFileWriter<ImageType>;
     WriterType::Pointer writer = WriterType::New();
-    writer->SetFileName("output.dcm");
+    writer->SetFileName(outputFilename);
     writer->SetInput(reader->GetOutput());
     writer->SetImageIO(dicomIO);
     writer->SetMetaDataDictionary(dictionary);
 
-    try
-    {
-        writer->Update();
-    }
-    catch(itk::ExceptionObject& ex)
-    {
-        std::cerr << ex <<std::endl;
-        return -3;
-    }
+    try { writer->Update(); }
+    catch(itk::ExceptionObject& ex) { std::cerr << ex <<std::endl; return -3;}
+
+    return 0;
 }
 
 int main(int argc, char **argv)
@@ -217,13 +174,13 @@ int main(int argc, char **argv)
     if(itksys::SystemTools::FileIsDirectory(argv[1]))
     {
         std::cout << "Processing dicom series\n";
-        seriesAnonymiser(argv[1], map_);
+        return seriesAnonymiser(argv[1], map_);
     }
     else 
     {
         std::cout << "Processing dicom\n";
-        dicomAnonymiser(argv[1], map_);
+        return dicomAnonymiser(argv[1], map_);
     }
 
-    return 0;
+        return 0;
 }
